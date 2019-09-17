0ac040d8e7be4c21b8a17d4a880a5c6c27c0a2ff
Tom HOYO <tom.hoyo@viacesi.fr>
2019-09-17 17:02:50 +0200

    testView : display

--- src/LaunchGame.py
+++ src/LaunchGame.py
@@ -1,1 +1,14 @@
 import pygame
+
+continuer = True
+
+pygame.init()
+ecran = pygame.display.set_mode((300, 200))
+
+while continuer:
+	for event in pygame.event.get():
+		if event.type == pygame.KEYDOWN:
+			continuer = False
+
+
+pygame.quit()
