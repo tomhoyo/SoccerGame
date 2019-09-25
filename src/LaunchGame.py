e366f84622a952cc8f455d4e8b1a9ff95c206b53
TomHOYO <tom.hoyo@viacesi.fr>
2019-09-26 00:21:34 +0200

    create ball class, move the player in a class

--- src/LaunchGame.py
+++ src/LaunchGame.py
@@ -1,52 +1,7 @@
 import pygame
-import time
-from Player import *
+from Controller import *
 
-continuer = True
-
-width = 1000
-height = 600
-
-speed = 1
-
-pygame.init()
-player1 = Player(50, 200)
-
-ecran = pygame.display.set_mode((width, height))
-icon = pygame.image.load("ressources/Soccer_Ball_icon.png")
-
-pygame.display.set_caption("SoccerGame")
-pygame.display.set_icon(icon)
-
-pygame.draw.rect(ecran, (255, 255, 255), (0, 0, width, height))
-ecran.blit(player1.imgPlayer, (player1.getX(), player1.getY()))
-
-limX = width - player1.getImgPlayer().get_width()
-limY = height - player1.getImgPlayer().get_height()
-
-z = q = s = d = 0
-
-while continuer:
-	keystate = pygame.key.get_pressed()
-	if keystate[pygame.K_w] and player1.getY() > 0:
-		player1.setY(player1.getY()-speed)
-	if keystate[pygame.K_s] and player1.getY() < limY:
-		player1.setY(player1.getY()+speed)
-	if keystate[pygame.K_a] and player1.getX() > 0:
-		player1.setX(player1.getX()-speed)
-	if keystate[pygame.K_d] and player1.getX() < limX:
-		player1.setX(player1.getX()+speed)
-
-	for event in pygame.event.get():
-
-		if event.type == pygame.KEYDOWN:
-
-			if event.key == pygame.K_f:
-				continuer = False
-
-	pygame.draw.rect(ecran, (255, 255, 255), (0, 0, width, height))
-	ecran.blit(player1.getImgPlayer(), (player1.getX(), player1.getY()))
-
-	pygame.display.flip()
+controller = Controller()
+controller.startGame()
 
 pygame.quit()
--- src/Player.py
+++ src/Player.py
@@ -5,10 +5,17 @@ class Player:
 	x = 0
 	y = 0
 	imgPlayer = pygame.image.load("ressources/player2.png")
+	speed = 0.5
 
-	def __init__(self, x, y):
+	limY = 0
+	limX = 0
+
+	def __init__(self, ctrl, x, y):
 		self.x = x
 		self.y = y
+
+		self.limX = ctrl.width - self.getImgPlayer().get_width()
+		self.limY = ctrl.height - self.getImgPlayer().get_height()
 
 	def getX(self):
 		return self.x
@@ -25,4 +32,32 @@ class Player:
 	def getImgPlayer(self):
 		return self.imgPlayer
 
+	def getSpeed(self):
+		return self.speed
 
+	def setlimY(self, limY):
+		self.limY = limY
+
+	def getlimY(self):
+		return self.limY
+
+	def setlimX(self, limX):
+		self.limX = limX
+
+	def getlimY(self):
+		return self.limX
+
+	def setSpeed(self, speed):
+		self.speed = speed
+
+	def moveUp(self):
+		self.setY(self.getY()-self.getSpeed())
+
+	def moveDown(self):
+		self.setY(self.getY()+self.getSpeed())
+
+	def moveRight(self):
+		self.setX(self.getX()+self.getSpeed())
+
+	def moveLeft(self):
+		self.setX(self.getX()-self.getSpeed())
